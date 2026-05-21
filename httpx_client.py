d4b60d6328684292f4358ea6fe0c574bf12625dc
Ivalg <pro102@list.ru>
2026-05-21 08:05:05 +0300

    Реализован базовый APIClient с CRUD методами

---
+++ httpx_client.py
@@ -0,0 +1,22 @@
+import httpx
+
+# Проходим аутентификацию
+login_payload = {
+    "email": "user1@example.com",
+    "password": "string"
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print("Login data:", login_response_data)
+
+# Инициализируем клиент
+client = httpx.Client(
+    base_url="http://localhost:8000/",
+    timeout=100,
+    headers={"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+)
+
+# Выполняем запрос с авторизацией
+get_user_response = client.get("api/v1/users/me")
+get_user_response_data = get_user_response.json()
+print("User data:", get_user_response_data)
